4818cca9141f788c453e087bc92b697bfa372f67
Robbie Demaegdt <robbie.demaegdt@skyline.be>
2024-04-09 13:33:11 +0200

    change target framework

--- Utils.TableCleanup/TableCleaner.cs
+++ Utils.TableCleanup/TableCleaner.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Skyline.DataMiner.Scripting;
 using Skyline.DataMiner.Utils.TableCleanup.Interfaces;
 using SLNetMessages = Skyline.DataMiner.Net.Messages;
